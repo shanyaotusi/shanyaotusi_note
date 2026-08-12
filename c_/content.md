db30809f2cd38c87e87f2429cc566a755f399a5f
shanyaotusi <136062023007@student.fjnu.edu.cn>
2026-08-12 13:25:21 +0800

    ...

--- c_/content.md
+++ c_/content.md
@@ -1,1 +1,5 @@
 # c语言笔记
+
+## malloc相关用法
+
+[c/c++如何获取malloc分配的内存空间大小\_获取malloc分配的内存大小-CSDN博客](https://blog.csdn.net/weixin_43147677/article/details/106801305)
--- data_structures/9.1_static_search.c
+++ data_structures/9.1_static_search.c
@@ -13,14 +13,19 @@ typedef struct {
 	Elemtype el;
 } dataNode, *dataList;
 
+typedef struct searchTable{
+	dataList ls;
+	int len;
+} *searchTable;
+
 int dataCmp(Elemtype a, Elemtype b) {
 	return a == b? 1:0;
 }
 
 dataList newDataList(int len) {
 	dataList ls = (dataList)malloc(len*sizeof(dataNode));
+	if(!ls) {puts("newDataList: overflow"); return NULL;}
 	int defaultArr[20] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
-	if(!ls) return -1;
 	puts("initial data list...");
 	for(int i = 1;i<=20;++i){
 		ls[i].key = defaultArr[i];
@@ -34,33 +39,67 @@ dataList freeDataList(dataList ls) {
 	return NULL;
 }
 
+searchTable newSearchTable(int len) {
+	searchTable tb = (searchTable)malloc(sizeof(*searchTable));
+	if(!tb) {puts("newSearchTable: overflow"); return NULL;}
+	tb->ls = newDataList(len);
+	tb->len = len;
+	return tb;
+}
+
+searchTable freeSearchTable(searchTable tb) {
+	if(tb) {
+		freeDataList(tb->ls);
+		free(tb);
+	}
+	return NULL;
+}
+
 //=================顺序查找====================
-dataNode* sqlSearch(dataList ls,int len, Keytype id) {
-	// len 数据长度，不包含头节点
-	if(!ls) {puts("sqlSearch: arg error!");return NULL;}
+dataNode* sqlSearch(searchTable tb, Keytype id) {
+	if(!tb) {puts("sqlSearch: invalid arg!");return NULL;}
 	// 设置哨兵
-	ls[0].key = id;
-	dataNode *ptr = ls+len;
+	tb->ls[0].key = id;
+	dataNode *ptr = tb->ls + tb->len;
 	while(ptr) {
-		if(ptr->key == ls->key)
+		if(ptr->key == tb->ls->key)
 			return ptr;
 		--ptr;
 	}
 	return ls;
 } 
 //=================折半查找====================
-dataNode* hlfSearch(dataList ls, int len, Keytype id) {
-	if(!ls) {puts("hlfSearch: arg error!");return NULL;}
+dataNode* hlfSearch(searchTable tb, Keytype id) {
+	if(!tb) {puts("hlfSearch: invalid arg!");return NULL;}
 	int left = 1, right = len;
 	while(left <= right){
 		int mid = (left + right)/2;
-		if(ls[mid].key == id) 
-			return ls+mid;
-		if(ls[mid].key > id)
+		if(tb->ls[mid].key == id) 
+			return tb->ls+mid;
+		if(tb->ls[mid].key > id)
 			right = mid;
 		else left = mid;
 	}
 	return NULL;
 }
 //=================分块查找====================
-typedef dataList indexTable;// 索引表
+typedef struct indexList {
+	Keytype key;
+	int index;
+} *indexList;
+// 索引表
+typedef struct indexTable {
+	indexList itb;
+	int len;
+} *indexTable;
+// 复合查找表
+typedef struct blockTable {
+	indexTable index;
+	dataList ls;
+	int len;
+} *blockTable;
+dataNode* blockSearch(blockTable tb, Keytype id) {
+	if(!ls) {puts("hlfSearch: arg error!");return NULL;}
+	
+	return NULL;
+}
